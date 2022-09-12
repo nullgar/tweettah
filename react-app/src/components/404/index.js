1c79828d1564ab4e83a936ed772f6175a245d49a
Jon Aguilar <technicianaguilar@gmail.com>
2022-09-12 00:13:19 -0700

    Fixed

--- react-app/src/components/404/index.js
+++ react-app/src/components/404/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './404.css'
 const Page404 = () => {
     return (
-        <div>
-            <h2>404 Page not found</h2>
+        <div className="page-404-div">
+            <h2 className="h2-404">404 Page not found</h2>
 
-            <p>Please go back to <Link to={'/'}>Home</Link></p>
+            <p className="p-404">Please go back to <Link className="link-404" to={'/'}>Home</Link></p>
         </div>
     )
 
